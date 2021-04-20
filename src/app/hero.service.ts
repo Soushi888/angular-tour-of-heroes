055aa6662886ab7924fe92a83a0944dbe038f9d1
Soushi888 <Sacha.pignot@protonmail.com>
2021-04-19 23:19:00 -0400

    get Heroes data asynchronously

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+
 import {Hero} from './Hero';
 import {HEROES} from './mock-heroes';
 
@@ -7,8 +9,8 @@ import {HEROES} from './mock-heroes';
 })
 
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 
   constructor() {
